f51fe3d7ec62f30ca26106c2eeaa2ae90b928e02
Pee <add.pee@hotmail.com>
2024-08-23 03:55:10 +0700

    assignment:values variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let surename = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurant = "Chef's Table by Chef James";
+const foodPrice = 15000;
+const totalRestaurant = 1500;
+
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+foodPrice = 5000;
+totalRestaurant = 2500;
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,6 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
 
-// Start coding here;
 
-let fahrenheit;
+
+let fahrenheit = (celsius x 1.8) + 32;
